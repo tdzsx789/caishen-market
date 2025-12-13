5bee04614883d28fb396e8e3b80059ad39e51284
nick5360639461 <>
2025-12-13 18:15:39 +0800

    feat: 框架创建

---
+++ src/pages/Welcome.tsx
@@ -0,0 +1,14 @@
+import { PageContainer } from '@ant-design/pro-components';
+import { useModel } from '@umijs/max';
+import { Card, theme } from 'antd';
+import React from 'react';
+
+const Welcome: React.FC = () => {
+  return (
+    <PageContainer>
+      11111
+    </PageContainer>
+  );
+};
+
+export default Welcome;
