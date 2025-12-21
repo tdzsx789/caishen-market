4f775fb758f678dbe546270fa818cc195ccd47fd
nick5360639461 <2772970991@qq.com>
2025-12-21 21:50:43 +0800

    feat: 删除国际化代码

--- src/global.tsx
+++ src/global.tsx
@@ -1,4 +1,3 @@
-import { useIntl } from '@umijs/max';
 import { Button, message, notification } from 'antd';
 import defaultSettings from '../config/defaultSettings';
 
@@ -23,7 +22,7 @@ const clearCache = () => {
 if (pwa) {
   // Notify user if offline now
   window.addEventListener('sw.offline', () => {
-    message.warning(useIntl().formatMessage({ id: 'app.pwa.offline' }));
+    message.warning('当前处于离线状态');
   });
 
   // Pop up a prompt on the page asking the user if they want to use the latest version
@@ -62,14 +61,12 @@ if (pwa) {
           reloadSW();
         }}
       >
-        {useIntl().formatMessage({ id: 'app.pwa.serviceworker.updated.ok' })}
+        刷新
       </Button>
     );
     notification.open({
-      message: useIntl().formatMessage({ id: 'app.pwa.serviceworker.updated' }),
-      description: useIntl().formatMessage({
-        id: 'app.pwa.serviceworker.updated.hint',
-      }),
+      message: '发现新版本',
+      description: '新版本已就绪，是否立即刷新？',
       btn,
       key,
       onClose: async () => null,
